c89b6b8b981185e5f473ddff67a67e8983fa6293
elija-feigl <elija.feigl@gmail.com>
2020-07-08 12:47:21 +0200

    adaptations & cleanup move writemrc() from Util to linkerapp method streamline public methods methodology cleanup ocerly complex code

--- linkerapp.py
+++ linkerapp.py
@@ -9,8 +9,11 @@ from pathlib import Path
 from IPython.display import display
 
 from core.linker import Linker
-from core.project import Project
+from core.project import Project, Files
 from core.pdbCorrection import PDB_Corr, Logic
+
+from linkerappUtil import (_create_voxel_mask, _mrc_cut_minimalbox,
+                           _get_mrc_properties)
 
 
 __authors__ = ["Elija Feigl"]
@@ -32,46 +35,51 @@ __email__ = "elija.feigl@tum.de"
 
 
 class Viewer(object):
-    def __init__(self, folder, files):
+    def __init__(self, folder, files: Files):
         name = files[0].stem
-        self.project = Project(input=Path(folder),
-                               output=Path(folder) / "analysis",
+        self.project = Project(folder=Path(folder),
                                name=name,
                                files=files,
                                )
         self.linker = Linker(self.project)
+        self.link = self.linker.create_linkage()
+        self.Hid2H = self.linker.design.design.structure_helices_map
+        self.Hcoor2H = self.linker.design.design.structure_helices_coord_map
         self.selection_scaffold = None
         self.selection_staples = None
 
     def select_by_helixandbase(self, helices, bases):
 
         self._parse_selection(bases, helices)
-        link = self.linker.create_linkage()
-
-        atoms_scaffold = mda.AtomGroup([], self.linker.fit.u)
-        atoms_staple = mda.AtomGroup([], self.linker.fit.u)
+        atoms_scaffold = mda.AtomGroup([], self.link.u)
+        atoms_staple = mda.AtomGroup([], self.link.u)
 
         for base in self.selection_scaffold:
-            atoms_scaffold += self.linker.fit.u.residues[
-                link.DidFid[link.DhpsDid[(base.h, base.p, True)]]].atoms
+            atoms_scaffold += self.link.u.residues[
+                self.link.DidFid[
+                    self.link.DhpsDid[(base.h, base.p, True)]
+                ]
+            ].atoms
 
         for base in self.selection_staples:
-            atoms_staple += self.linker.fit.u.residues[link.DidFid[
-                link.DhpsDid[(base.h, base.p, False)]]].atoms
+            atoms_staple += self.link.u.residues[
+                self.link.DidFid[
+                    self.link.DhpsDid[(base.h, base.p, False)]
+                ]
+            ].atoms
 
         atoms_selection = atoms_scaffold + atoms_staple
 
-        return atoms_selection, atoms_scaffold, atoms_staple, link.Dcolor
+        return atoms_selection, self.link.Dcolor
 
     def _parse_selection(self, base_selection, helix_selection):
         # get helices from design
-        helices = []
-        for idx in helix_selection:
-            helix = self.linker.design.design.structure_helices_map[idx]
-            helices.append(helix)
+        helices = [self.Hid2H[idx] for idx in helix_selection]
+
         # get bases from helices
         self.selection_scaffold = []
         self.selection_staples = []
+        # TODO: test without deletions
         for helix in helices:
             selection_scaffold_add = [
                 base for base in helix.scaffold_bases
@@ -85,58 +93,34 @@ class Viewer(object):
             self.selection_scaffold += selection_scaffold_add
             self.selection_staples += selection_staples_add
 
-        return
-
     def make_sliders(self):
-
         layout_w = widgets.Layout(width='900px', height='70px')
         layout_h = widgets.Layout(width='100px')
         layout_box = widgets.Layout(width='1000px', border='1px solid black')
         layout_button = widgets.Layout(width='30px', height='30px')
-        design = self.linker.design.design
 
         base_p = [base.p for base in self.linker.design.allbases]
-        maxb = max(base_p)
-        minb = min(base_p)
-        row = [coor[0] for coor, _ in
-               design.structure_helices_coord_map.items()]
-        minr = min(row)
-        maxr = max(row)
-        col = [coor[1] for coor, _ in
-               design.structure_helices_coord_map.items()]
-        minc = min(col)
-        maxc = max(col)
+        minb, maxb = min(base_p), max(base_p)
+        row = [coor[0] for coor in self.Hcoor2H.keys()]
+        minr, maxr = min(row), max(row)
+        col = [coor[1] for coor in self.Hcoor2H.keys()]
+        minc, maxc = min(col), max(col)
 
-        lattice = ("square" if design.lattice_type == 0
+        lattice = ("square" if self.linker.design.design.lattice_type == 0
                    else "honeycomb")
 
         helix_buttons = []
-        if lattice == "square":
+        if lattice in ["square", "honeycomb"]:  # TODO improve honeycomb
             for r in range(minr, maxr + 1):
                 row = []
                 for c in range(minc, maxc + 1):
-                    try:
-                        id = str(
-                            design.structure_helices_coord_map[(r, c)].id)
-                    except KeyError:
-                        id = ""
-
+                    h = self.Hcoor2H.get((r, c), None)
+                    h_id = "" if h is None else str(h.id)
                     but = widgets.ToggleButton(
-                        description=id, layout=layout_button)
-                    row.append(but)
-                helix_buttons.append(row)
-        else:  # TODO: -low- improve
-            for r in range(minr, maxr + 1):
-                row = []
-                for c in range(minc, maxc + 1):
-                    try:
-                        id = str(
-                            design.structure_helices_coord_map[(r, c)].id)
-                    except KeyError:
-                        id = ""
-
-                    but = widgets.ToggleButton(
-                        description=id, layout=layout_button)
+                        description=h_id,
+                        layout=layout_button,
+                        disabled=(h is None),
+                    )
                     row.append(but)
                 helix_buttons.append(row)
 
@@ -177,49 +161,70 @@ class Viewer(object):
         sliders_w = widgets.VBox([buttons_h, slider_b])
         sliders = widgets.Box([sliders_w, slider_c], layout=layout_box)
         display(sliders)
-
         return (helix_buttons, slider_b, slider_c)
 
     def eval_sliders(self, helix_buttons, slider_b, slider_c):
 
         selection_bases = range(slider_b.lower, slider_b.upper + 1)
 
-        selection_helices = []
         flat_list = [item for sublist in helix_buttons for item in sublist]
-        for button in flat_list:
-            if button.value:
-                try:
-                    selection_helices.append(int(button.description))
-                except ValueError:
-                    pass
+        selection_helices = [int(b.description) for b in flat_list
+                             if (not b.disabled and b.value)
+                             ]
+
         with mrcfile.open(self.project.files.mrc, mode='r+') as mrc:
             voxel_size = mrc.voxel_size.x
         context = int(slider_c.value / voxel_size) + 1
 
         return (selection_helices, selection_bases), context
 
-    def writepdb(self, atoms_selection, single_frame=True, frame=-1):
-        inp = self.project.input
-        path_in = inp / "{}-tmp.pdb".format(self.project.name)
-        path_corr = inp / "{}-tmp_chim.pdb".format(self.project.name)
+    def writepdb(self, atoms_selection, name, single_frame=True, frame=-1, chimeraX=True):
+        inp = self.project.folder
+        path_in = inp / "{}.pdb".format(name)
+        path_corr = inp / "{}_chim.pdb".format(name)
 
         with mda.Writer(path_in, multiframe=True,
                         n_atoms=atoms_selection.n_atoms) as W:
             if single_frame:
-                self.linker.fit.u.trajectory[frame]
+                self.link.u.trajectory[frame]
                 W.write(atoms_selection)
             else:
-                for _ in self.linker.fit.u.trajectory:
+                for _ in self.link.u.trajectory:
                     W.write(atoms_selection)
-        logic = Logic(keep_header=False,
-                      remove_H=False,
-                      )
-        pdb_Corr = PDB_Corr(reverse=False)
-        with open(path_in, "r") as file_init:
-            newFile = pdb_Corr.correct_pdb(pdb_file=file_init,
-                                           logic=logic,
-                                           )
-        with open(path_corr, "w") as file_corr:
-            file_corr.write(newFile)
 
-        return
+        if chimeraX:
+            logic = Logic(keep_header=False,
+                          remove_H=False,
+                          )
+            pdb_Corr = PDB_Corr(reverse=False)
+            with open(path_in, "r") as file_init:
+                newFile = pdb_Corr.correct_pdb(pdb_file=file_init,
+                                               logic=logic,
+                                               )
+            with open(path_corr, "w") as file_corr:
+                file_corr.write(newFile)
+
+    def writemrc(self, atoms_selection, name: str, context=4, cut_box=True):
+        if not len(atoms_selection):
+            print("EXIT - no atoms in this selection")
+            return
+
+        inp = self.project.folder
+        path_out = inp / "{}.mrc".format(name)
+        u = atoms_selection.universe
+        u.trajectory[-1]
+
+        with mrcfile.open(self.project.files.mrc) as mrc:
+            voxel_size, grid, origin, full_data = _get_mrc_properties(mrc)
+
+        data_mask = _create_voxel_mask(
+            atoms_selection, grid, origin, voxel_size, context)
+        data = full_data * data_mask
+
+        if cut_box:
+            data, origin, voxel_size = _mrc_cut_minimalbox(data, origin, voxel_size)
+
+        with mrcfile.new(path_out, overwrite=True) as mrc_out:
+            mrc_out.set_data(data.transpose())
+            mrc_out._set_voxel_size(*voxel_size)
+            mrc_out.header["origin"] = tuple(origin)
--- linkerappUtil.py
+++ linkerappUtil.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import mrcfile
 import numpy as np
 import nglview as nv
 
 from PyQt5.QtWidgets import QFileDialog
 from pathlib import Path
 
+from MDAnalysis.core.groups import AtomGroup
 
 __authors__ = ["Elija Feigl"]
 __version__ = "1.0"
@@ -17,92 +17,6 @@ __descr__ = "VIEWERTOOL: utility scripts for viewer-tool"
 __status__ = "Development"
 __maintainer__ = "Elija Feigl"
 __email__ = "elija.feigl@tum.de"
-
-
-def mrc(atoms_selection, infile: Path, outfile: Path, context=4, cut_box=True):
-    if not len(atoms_selection):
-        print("EXIT - no atoms in this selection")
-        return
-
-    u = atoms_selection.universe
-    u.trajectory[-1]
-    with mrcfile.open(infile) as mrc:
-        voxel_size, grid, origin, full_data = get_mrc_properties(mrc)
-
-    data_mask = create_voxel_mask(
-        atoms_selection, grid, origin, voxel_size, context)
-    data = full_data * data_mask
-
-    if cut_box:
-        data, origin, voxel_size = mrc_cut_minimalbox(data, origin, voxel_size)
-
-    with mrcfile.new(outfile, overwrite=True) as mrc_out:
-        mrc_out.set_data(data.transpose())
-        mrc_out._set_voxel_size(*voxel_size)
-        mrc_out.header["origin"] = tuple(origin)
-
-
-def create_voxel_mask(atoms_selection, grid, origin, voxel_size, context):
-    data_mask = np.zeros(grid, dtype=np.float32)
-    v_context = np.full(3, context / voxel_size).astype(int) + 1
-    v_context_x, v_context_y, v_context_z = v_context
-
-    grid_positions = np.rint(
-        ((atoms_selection.positions - origin) / voxel_size)).astype(int)
-    for pos in grid_positions:
-        x, y, z = pos[0], pos[1], pos[2]  # fast to slow axis
-        data_mask[x - v_context_x: x + v_context_x,
-                  y - v_context_y: y + v_context_y,
-                  z - v_context_z: z + v_context_z
-                  ] = 1.
-    return data_mask
-
-
-def get_mrc_properties(mrc):
-    o = np.array(mrc.header["origin"])
-    origin = np.array([o["x"], o["y"], o["z"]])
-    c = np.array(mrc.header["cella"])
-    cell = np.array([c["x"], c["y"], c["z"]])
-    grid = np.array(
-        [mrc.header["nx"], mrc.header["ny"], mrc.header["nz"]])
-    data = mrc.data.transpose()
-    voxel_size = cell / grid
-
-    return (voxel_size, grid, origin, data)
-
-
-def mrc_cut_minimalbox(data, m_origin, m_spacing):
-    idx_data = np.nonzero(data)
-
-    x_min, x_max = np.min(idx_data[0]), np.max(idx_data[0])
-    y_min, y_max = np.min(idx_data[1]), np.max(idx_data[1])
-    z_min, z_max = np.min(idx_data[2]), np.max(idx_data[2])
-
-    xyz_diff = max(x_max - x_min, y_max - y_min, z_max - z_min)
-    x_pad = 0.5 * (xyz_diff + x_min - x_max)
-    y_pad = 0.5 * (xyz_diff + y_min - y_max)
-    z_pad = 0.5 * (xyz_diff + z_min - z_max)
-    x_low = int(x_pad) if (x_pad % 1.) == 0. else int(x_pad) + 1
-    y_low = int(y_pad) if (y_pad % 1.) == 0. else int(y_pad) + 1
-    z_low = int(z_pad) if (z_pad % 1.) == 0. else int(z_pad) + 1
-
-    data_small = np.zeros(
-        (xyz_diff, xyz_diff, xyz_diff), dtype=np.float32)
-    data_small[x_low: -int(x_pad) or None, y_low: -int(y_pad) or None,
-               z_low: -int(z_pad) or None] = data[x_min: x_max,
-                                                  y_min: y_max,
-                                                  z_min: z_max]
-
-    # cumpute new origin
-    origin = (m_origin + ((x_min - x_low) * m_spacing[0],
-                          (y_min - y_low) * m_spacing[1],
-                          (z_min - z_low) * m_spacing[2]
-                          )
-              )
-    grid = np.shape(data_small)
-    cell = grid * m_spacing
-    spacing = (cell / grid)
-    return data_small, origin, spacing
 
 
 def fit_widget(atoms_selection, atoms_scaffold, atoms_staple, path,
@@ -162,3 +76,66 @@ def gui_fileDialog(wd='./', filter="all files (*)"):
 def gui_folderDialog(wd='./'):
     """Select a file via a dialog and return the file name."""
     return QFileDialog.getExistingDirectory(None, "Choose folder...", wd)
+
+
+def _create_voxel_mask(atoms_selection, grid, origin, voxel_size, context):
+    data_mask = np.zeros(grid, dtype=np.float32)
+    v_context = np.full(3, context / voxel_size).astype(int) + 1
+    v_context_x, v_context_y, v_context_z = v_context
+
+    grid_positions = np.rint(
+        ((atoms_selection.positions - origin) / voxel_size)).astype(int)
+    for pos in grid_positions:
+        x, y, z = pos[0], pos[1], pos[2]  # fast to slow axis
+        data_mask[x - v_context_x: x + v_context_x,
+                  y - v_context_y: y + v_context_y,
+                  z - v_context_z: z + v_context_z
+                  ] = 1.
+    return data_mask
+
+
+def _get_mrc_properties(mrc):
+    o = np.array(mrc.header["origin"])
+    origin = np.array([o["x"], o["y"], o["z"]])
+    c = np.array(mrc.header["cella"])
+    cell = np.array([c["x"], c["y"], c["z"]])
+    grid = np.array(
+        [mrc.header["nx"], mrc.header["ny"], mrc.header["nz"]])
+    data = mrc.data.transpose()
+    voxel_size = cell / grid
+
+    return (voxel_size, grid, origin, data)
+
+
+def _mrc_cut_minimalbox(data, m_origin, m_spacing):
+    idx_data = np.nonzero(data)
+
+    x_min, x_max = np.min(idx_data[0]), np.max(idx_data[0])
+    y_min, y_max = np.min(idx_data[1]), np.max(idx_data[1])
+    z_min, z_max = np.min(idx_data[2]), np.max(idx_data[2])
+
+    xyz_diff = max(x_max - x_min, y_max - y_min, z_max - z_min)
+    x_pad = 0.5 * (xyz_diff + x_min - x_max)
+    y_pad = 0.5 * (xyz_diff + y_min - y_max)
+    z_pad = 0.5 * (xyz_diff + z_min - z_max)
+    x_low = int(x_pad) if (x_pad % 1.) == 0. else int(x_pad) + 1
+    y_low = int(y_pad) if (y_pad % 1.) == 0. else int(y_pad) + 1
+    z_low = int(z_pad) if (z_pad % 1.) == 0. else int(z_pad) + 1
+
+    data_small = np.zeros(
+        (xyz_diff, xyz_diff, xyz_diff), dtype=np.float32)
+    data_small[x_low: -int(x_pad) or None, y_low: -int(y_pad) or None,
+               z_low: -int(z_pad) or None] = data[x_min: x_max,
+                                                  y_min: y_max,
+                                                  z_min: z_max]
+
+    # cumpute new origin
+    origin = (m_origin + ((x_min - x_low) * m_spacing[0],
+                          (y_min - y_low) * m_spacing[1],
+                          (z_min - z_low) * m_spacing[2]
+                          )
+              )
+    grid = np.shape(data_small)
+    cell = grid * m_spacing
+    spacing = (cell / grid)
+    return data_small, origin, spacing
